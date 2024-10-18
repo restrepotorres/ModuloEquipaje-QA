021bf1f9a2eb4014a5dfeefd206ad77f830dcd57
restrepoTorres <vmanuel.restrepo@udea.edu.co>
2024-10-17 22:37:32 -0500

    tuve f

--- src/test/java/com/example/moduloEquipaje/ModuloEquipaje/ModuloEquipajeApplicationTests.java
+++ src/test/java/com/example/moduloEquipaje/ModuloEquipaje/ModuloEquipajeApplicationTests.java
@@ -2,9 +2,12 @@ package com.example.moduloEquipaje.ModuloEquipaje;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test") // Indica que se debe usar el perfil de test
+
 class ModuloEquipajeApplicationTests {
 
 	@Test
